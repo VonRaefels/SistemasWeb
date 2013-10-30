4bef405133b11ad1c4b1293f42f55bda9458f961
VonRaefels <jorgemadridportillo@gmail.com>
2013-10-30 12:39:51 +0100

    Added author

--- Examen/src/es/ceura/web/quiz/loaders/QuizLoader.java
+++ Examen/src/es/ceura/web/quiz/loaders/QuizLoader.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 import es.ceura.web.quiz.models.Answer;
 import es.ceura.web.quiz.models.Question;
 import es.ceura.web.quiz.models.Quiz;
-
+    /*
+     * Jorge Madrid Portillo
+     */
 public class QuizLoader {
 
 	public static Quiz loadQuiz(String path) throws FileNotFoundException,
